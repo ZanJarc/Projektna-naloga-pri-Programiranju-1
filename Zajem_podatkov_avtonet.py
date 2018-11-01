c808fce2bf0d124fd8b2c946f9f37aa956ee1875
Žan Jarc <zan.jarc@gmail.com>
2018-11-01 09:50:29 +0100

    Dodan zajem in podatki

--- Zajem_podatkov_avtonet.py
+++ Zajem_podatkov_avtonet.py
@@ -2,15 +2,22 @@ import requests
 import re
 import os
 import csv
+import json
+import datetime
 
-top_100_spletna_stran = 'https://www.avto.net/Ads/results_100.asp?oglasrubrika=1&prodajalec=2'
-directory_mapa = 'Projetka_naloga_Programiranje_1'
-podatki_html = 'podatki.html'
-podatki_csv = 'podatki.csv'
+
+avtonet_url = 'https://www.avto.net/Ads/results_100.asp?oglasrubrika=1&prodajalec=2'
+directory_mapa = 'Mapa s podatki'
+izlusceni_oglasi = 'oglasi.html'
+podatki_html = 'HTMLpodatki.html'
+podatki_csv = 'CSVpodatki.csv'
+podatki_json = 'JSONpodatki.json'
+now = datetime.datetime.now()
 
 def download_url_to_string(url):
     try:
         r = requests.get(url)
+        r.encoding = 'windows-1250'
     except requests.exceptions.ConnectionError:
         print('Stran ne obstaja!')
         return ''
@@ -20,7 +27,7 @@ def download_url_to_string(url):
 def save_string_to_file(text, directory, filename):
     os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, filename)
-    with open(path, 'w', encoding='utf-8') as file_out:
+    with open(path, 'w', encoding="windows-1250") as file_out:
         file_out.write(text)
     return None
 
@@ -29,7 +36,8 @@ def save_string_to_file(text, directory, filename):
 def save_frontpage(url, directory, filename):
     os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, filename)
-    with open(path, 'w', encoding='utf-8') as datoteka:
+    vsebina = download_url_to_string(url)
+    with open(path, 'w', encoding='windows-1250') as datoteka:
         datoteka.write(vsebina)
     print('shranjeno!')
 
@@ -37,7 +45,7 @@ def save_frontpage(url, directory, filename):
 
 def read_file_to_string(directory, filename):
     path = os.path.join(directory, filename)
-    with open(path, encoding='utf-8') as datoteka:
+    with open(path, encoding='windows-1250') as datoteka:
         return datoteka.read()
 
 
@@ -47,9 +55,9 @@ def page_to_ads(directory, filename):
     vsebina = read_file_to_string(directory, filename)
     seznam_oglasov = []
     vzorec = (
-            r'<div class="ad.*?">'
+            r'<!--------------------- DESCRIPTION  ------------------->'
             r'.*?'
-            r'<div class="clear"></div>'
+            r'<!-- START BIG BANNER -->'
             )
     for ujemanje in re.finditer(vzorec, vsebina, re.DOTALL):
         nas_oglas = ujemanje.group(0)
@@ -57,34 +65,41 @@ def page_to_ads(directory, filename):
     return seznam_oglasov
 
 
-vzorec = re.compile(
-    r'<table><tr><td><a title=(?P<ime>.*?) href=.*?'
-    r'</a></h3>(?P<opis>.*?) <div class="additionalInfo">.*?'
-    r'<div class="price">(?P<cena>.*?)</div>.*?',
-    re.DOTALL
-)
+
+
+
+
+
 
 
 
 def izloci_podatke_oglasa(ujemanje_oglasa):
+    
     podatki_oglasa = ujemanje_oglasa.groupdict()
-    podatki_oglasa['opis'] = podatki_oglasa['opis'].strip()
-    podatki_oglasa['ime'] = podatki_oglasa['ime'].strip()
-    podatki_oglasa['ime'] = podatki_oglasa['ime'].strip('</span>')
+    podatki_oglasa['cas'] = (now.day, now.month)
     return podatki_oglasa
 
-podatki_oglasov = []
+
+
+
 
 
 def get_dict_from_ad_block(directory, filename):
+    podatki_oglasov = []
     '''Build a dictionary containing the name, description and price
     of an ad block.'''
     seznam_oglasov = page_to_ads(directory, filename)
-    izlusceno = izloci_podatke_oglasa(seznam_oglasov)
+    vsebina = read_file_to_string(directory, filename)
+    vzorec = re.compile(
+    r'<a class="Adlink" href=".*">\n\n<span>(?P<ime>.*)</span>\n\n</a>.*?'
+    r'<li>Letnik 1.registracije:(?P<letnik>\d*)</li>(\n){2}\s*?<li>(?P<kilometrina>.*)?</li><li>(?P<motor>.*)</li><li>(?P<menjalnik>.*)</li>.*?',
+    re.DOTALL
+    )
     for oglas in seznam_oglasov:
         for ujemanje in vzorec.finditer(oglas):
-            podatki_oglasov.append(izloci_podatke_oglasa(ujemanje))
+                podatki_oglasov.append(izloci_podatke_oglasa(ujemanje))
     return podatki_oglasov
+
 
 # Definirajte funkcijo, ki sprejme ime in lokacijo datoteke, ki vsebuje
 # besedilo spletne strani, in vrne seznam slovarjev, ki vsebujejo podatke o
@@ -95,3 +110,36 @@ def ads_from_file(directory, filename):
     slovar_oglasov = get_dict_from_ad_block(directory, filename)
     '''Parse the ads in filename/directory into a dictionary list.'''
     return slovar_oglasov
+
+
+"""def write_csv(fieldnames, rows, directory, filename):
+    '''Write a CSV file to directory/filename. The fieldnames must be a list of
+    strings, the rows a list of dictionaries each mapping a fieldname to a
+    cell-value.'''
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w', encoding='utf-8') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+    return print('napisal sem csv!')"""
+
+def zapisi_csv(slovarji, imena_polj, directory, filename):
+    '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w', encoding='utf-8') as csv_datoteka:
+        writer = csv.DictWriter(csv_datoteka, fieldnames=imena_polj)
+        writer.writeheader()
+        for slovar in slovarji:
+            writer.writerow(slovar)
+    print('Napisal sem csv!')
+
+
+def zapisi_json(slovarji, directory, filename):
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w', encoding='utf-8') as json_datoteka:
+        json.dump(slovarji, json_datoteka, indent=5, ensure_ascii=False)
+
