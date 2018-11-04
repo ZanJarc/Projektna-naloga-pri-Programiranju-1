ec0f9957a1420bc330178a4db702bd93b1f53118
Žan Jarc <zan.jarc@gmail.com>
2018-11-04 03:18:03 +0100

    Boljsi zajem
    Izboljsal zajem podatkov

---
+++ Zajem_imen.py
@@ -0,0 +1,112 @@
+import requests
+import re
+import os
+import csv
+import json
+
+avtonet_search_url = 'https://www.avto.net/'
+directory_mapa = 'Mapa s podatki'
+izluscena_imena = 'imena.html'
+podatki_html = 'HTMLpodatki_imena.html'
+podatki_csv = 'CSVpodatki_imena.csv'
+
+def download_url_to_string(url):
+    try:
+        r = requests.get(url)
+        r.encoding = 'windows-1250'
+    except requests.exceptions.ConnectionError:
+        print('Stran ne obstaja!')
+        return ''
+    return r.text
+
+
+def save_string_to_file(text, directory, filename):
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w', encoding='utf-8') as file_out:
+        file_out.write(text)
+    return None
+
+
+
+def save_frontpage(url, directory, filename):
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    vsebina = download_url_to_string(url)
+    with open(path, 'w', encoding='utf-8') as datoteka:
+        datoteka.write(vsebina)
+    print('shranjeno!')
+
+def read_file_to_string(directory, filename):
+    path = os.path.join(directory, filename)
+    with open(path, encoding='utf-8') as datoteka:
+        return datoteka.read()
+
+
+#dobimo samo tisti del html, kjer so imena
+def page_to_names(directory, filename):
+    vsebina = read_file_to_string(directory, filename)
+    names = []
+    vzorec = (
+            r'<option value="">Izberite znamko</option>'
+            r'.*?'
+            r'</select>'
+    )
+    for ujemanje in  re.finditer(vzorec, vsebina, re.DOTALL):
+        names.append(ujemanje.group(0))
+    return names
+
+
+def izlusci_znamke(directory, filename):
+    list_of_names = []
+    names = page_to_names(directory, filename)
+    vzorec = (
+        r'<option value=".+?">(?P<ime>.*?)</option>'
+    )
+    for ujemanje in re.finditer(vzorec, names[0], re.DOTALL):
+        list_of_names.append(ujemanje.group('ime'))
+    return list_of_names   
+
+
+#dobimo samo tisti del html, ki vsebuje podatke o moznih modelih
+def page_to_models(directory, filename):
+    vsebina = read_file_to_string(directory, filename)
+    text = []
+    vzorec = (
+            r'<select id="model" name="model">'
+            r'.*?'
+            r'</select>'
+    )
+    for ujemanje in  re.finditer(vzorec, vsebina, re.DOTALL):
+        text.append(ujemanje.group(0))
+    return text
+
+
+#s tem dobimo vse mozne kombinacije
+def izlusci_modele(directory, filename):
+    list_of_models = []
+    models = page_to_models(directory, filename)
+    vzorec = (
+        '<option value=".+?" class="(?P<znamka>.*?)">(?P<model>.*?)</option>'
+    )
+    for ujemanje in re.finditer(vzorec, models[0], re.DOTALL):
+        if ujemanje.group('model') != "modela ni na seznamu":
+            list_of_models.append('{} {}'.format(ujemanje.group('znamka'), ujemanje.group('model')))
+    return list_of_models
+
+#modeli urejeni po znamkah
+def modeli_znamk(directory, filename):
+    list_of_models = {}
+    models = page_to_models(directory, filename)
+    vzorec = (
+        '<option value=".+?" class="(?P<znamka>.*?)">(?P<model>.*?)</option>'
+    )
+    for ujemanje in re.finditer(vzorec, models[0], re.DOTALL):
+        if ujemanje.group('model') != "modela ni na seznamu":
+            if ujemanje.group('znamka') in list_of_models:
+                list_of_models[ujemanje.group('znamka')].append(ujemanje.group('model'))
+            else:
+                list_of_models[ujemanje.group('znamka')] = [ujemanje.group('model')]
+    return list_of_models
+   
+                
--- Zajem_podatkov_avtonet.py
+++ Zajem_podatkov_avtonet.py
@@ -15,6 +15,7 @@ podatki_json = 'JSONpodatki.json'
 slovarji_html = 'slovarji.html'
 now = datetime.datetime.now()
 
+
 def download_url_to_string(url):
     try:
         r = requests.get(url)
@@ -85,11 +86,29 @@ def save_ads(directory, podatki, filename):
 
 
 
-
+#kako zaustavim for zanko
 def izloci_podatke_oglasa(ujemanje_oglasa):
     podatki_oglasa = ujemanje_oglasa.groupdict()
     podatki_oglasa['cas'] = (now.day, now.month)
     podatki_oglasa['cena'] = podatki_oglasa['cena'] + '€'
+    for model in MODELI:
+        for ujemanje in re.finditer(model, podatki_oglasa['ime'], re.DOTALL):
+            if ujemanje.group(0):
+                podatki_oglasa['ime'] = ujemanje.group(0)
+    return podatki_oglasa
+
+
+
+def izloci_podatke_oglasa_BOLJSE(ujemanje_oglasa):
+    podatki_oglasa = ujemanje_oglasa.groupdict()
+    podatki_oglasa['cas'] = (now.day, now.month)
+    podatki_oglasa['cena'] = podatki_oglasa['cena'] + '€'
+    for znamka in MODELI_BOLJSE:
+        if znamka in podatki_oglasa['ime']:
+            for model in MODELI_BOLJSE[znamka]:
+                if znamka + ' ' + model in podatki_oglasa['ime']:
+                    print('spremenil bom {} v {}'.format(podatki_oglasa['ime'], (znamka + ' ' + model)))
+                    podatki_oglasa['ime'] = '{} {}'.format(znamka, model)
     return podatki_oglasa
 
 
@@ -116,6 +135,26 @@ def get_dict_from_ad_block(directory, filename):
 
 
 
+def get_dict_from_ad_block_BOLJSE(directory, filename):
+    podatki_oglasov = []
+    '''Build a dictionary containing the name, description and price
+    of an ad block.'''
+    seznam_oglasov = page_to_ads(directory, filename)
+    vsebina = read_file_to_string(directory, filename)
+    vzorec = re.compile(
+    r'<a class="Adlink" href=".*">\n\n<span>(?P<ime>.*)</span>\n\n</a>.*?'
+    r'<li>Letnik 1.registracije:(?P<letnik>\d*)</li>(\n){2}\s*?<li>(?P<kilometrina>.*)?</li><li>(?P<motor>.*)</li><li>(?P<menjalnik>.*)</li>.*?'
+    r'EUR=(?P<cena>\d*)',
+    re.DOTALL
+    )
+    for oglas in seznam_oglasov:
+        for ujemanje in vzorec.finditer(oglas):
+                podatki_oglasov.append(izloci_podatke_oglasa_BOLJSE(ujemanje))
+    return podatki_oglasov
+
+
+
+
 
 def pridobi_ceno(directory, filename):
     cene = []
@@ -129,20 +168,88 @@ def pridobi_ceno(directory, filename):
     for ujemanje in vzorec.finditer(vsebina):
         cene.append(ujemanje.groupdict())
     return cene
-# Definirajte funkcijo, ki sprejme ime in lokacijo datoteke, ki vsebuje
-# besedilo spletne strani, in vrne seznam slovarjev, ki vsebujejo podatke o
-# vseh oglasih strani.
+
+
 
 
 def ads_from_file(directory, filename):
     slovar_oglasov = get_dict_from_ad_block(directory, filename)
-    '''Parse the ads in filename/directory into a dictionary list.'''
     return slovar_oglasov
 
 
 
+#dobimo samo tisti del html, kjer so imena
+def page_to_names(directory, filename):
+    vsebina = read_file_to_string(directory, filename)
+    names = []
+    vzorec = (
+            r'<option value="">Izberite znamko</option>'
+            r'.*?'
+            r'</select>'
+    )
+    for ujemanje in  re.finditer(vzorec, vsebina, re.DOTALL):
+        names.append(ujemanje.group(0))
+    return names
+
+
+
+def izlusci_znamke(directory, filename):
+    list_of_names = []
+    names = page_to_names(directory, filename)
+    vzorec = (
+        r'<option value=".+?">(?P<ime>.*?)</option>'
+    )
+    for ujemanje in re.finditer(vzorec, names[0], re.DOTALL):
+        list_of_names.append(ujemanje.group('ime'))
+    return list_of_names   
+
+
+#dobimo samo tisti del html, ki vsebuje podatke o moznih modelih
+def page_to_models(directory, filename):
+    vsebina = read_file_to_string(directory, filename)
+    text = []
+    vzorec = (
+            r'<select id="model" name="model">'
+            r'.*?'
+            r'</select>'
+    )
+    for ujemanje in  re.finditer(vzorec, vsebina, re.DOTALL):
+        text.append(ujemanje.group(0))
+    return text
+
+
+#s tem dobimo vse mozne kombinacije
+def izlusci_modele(directory, filename):
+    list_of_models = []
+    models = page_to_models(directory, filename)
+    vzorec = (
+        '<option value=".+?" class="(?P<znamka>.*?)">(?P<model>.*?)</option>'
+    )
+    for ujemanje in re.finditer(vzorec, models[0], re.DOTALL):
+        if ujemanje.group('model') != "modela ni na seznamu":
+            list_of_models.append('{} {}'.format(ujemanje.group('znamka'), ujemanje.group('model')))
+    return list_of_models
+
+
+#modeli urejeni po znamkah
+def modeli_znamk(directory, filename):
+    list_of_models = {}
+    models = page_to_models(directory, filename)
+    vzorec = (
+        '<option value=".+?" class="(?P<znamka>.*?)">(?P<model>.*?)</option>'
+    )
+    for ujemanje in re.finditer(vzorec, models[0], re.DOTALL):
+        if ujemanje.group('model') != "modela ni na seznamu":
+            if ujemanje.group('znamka') in list_of_models:
+                list_of_models[ujemanje.group('znamka')].append(ujemanje.group('model'))
+            else:
+                list_of_models[ujemanje.group('znamka')] = [ujemanje.group('model')]
+    return list_of_models
+   
+
+
+
 def zapisi_csv(slovarji, imena_polj, directory, filename):
-    '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
     os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, filename)
     with open(path, 'w', encoding='utf-8') as csv_datoteka:
@@ -158,4 +265,7 @@ def zapisi_json(slovarji, directory, filename):
     path = os.path.join(directory, filename)
     with open(path, 'w', encoding='utf-8') as json_datoteka:
         json.dump(slovarji, json_datoteka, indent=5, ensure_ascii=False)
+    print('napisal sem json file!')
 
+MODELI = izlusci_modele(directory_mapa, 'search_stran.html')
+MODELI_BOLJSE = modeli_znamk(directory_mapa, 'search_stran.html')
